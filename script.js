e109d122f3d9f5f3395ad35e4fc7b4ac792662e6
Jhensen Agni <jagni001@ucr.edu>
2019-11-16 09:16:28 -0800

    Added properly functioning timer

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 
-// when the start button is clicked(getElementById)
-    // start countdown timer
-    // pick random question from list of questions
-var totalQuizTime = 25;
+var startButton = document.getElementById("start-quiz");
+var Timer = document.getElementById("timer");
+var questions = [
+                {
+                    title: "Commonly used data types DO NOT include:",
+                    choices: ["strings", "booleans", "alerts", "numbers"],
+                    answer: "alerts"
+                },
+                {
+                    title: "The condition in an if / else statement is enclosed within ____.",
+                    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+                    answer: "parentheses"
+                },
+                {
+                    title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+                    choices: ["last()","put()","push()","None of the above"],
+                    answer: "push"
+                },
 
 
-    
-document.getElementById("start-quiz").addEventListener("click",function()
+                ];
+
+
+startButton.addEventListener("click",function(totalQuizTime)
     {
         //start the timer
-        
-        var countdownTimer = setInterval(function()
-        {
-            totalQuizTime = totalQuizTime - 1;
-            document.getElementById("timer").innerHTML = "Time Remaining: " + totalQuizTime + " seconds";
-        
-        
+        var totalQuizTime = 10;
+        var quizIntroObj = document.getElementById("quiz-intro");
 
-            if(totalQuizTime < 20)
-                {
-                    document.getElementById("timer").style.color = "orange";
-                }
-            else if(totalQuizTime < 10)
-                {
+        var countdownTimer = setInterval(function()
+            {
+                totalQuizTime = totalQuizTime - 1;
+                document.getElementById("timer").innerHTML = "Time Remaining: " + totalQuizTime + " seconds";
+                document.getElementById("timer").style = "white";
+                if (totalQuizTime <= 20  && totalQuizTime > 10)
+                    {
+                        document.getElementById("timer").style.color = "orange";
+                    }                    
+                if(totalQuizTime <= 10 && totalQuizTime>0)
+                    {
                     document.getElementById("timer").style.color = "red";
-                }
-            else if(totalQuizTime === 0)
-                {   
-                    clearInterval(countdownTimer);
-                    document.getElementById("timer").style.color = "maroon";
-                }
-    
-        }, 1000);
+                    }            
+                if (totalQuizTime==0)
+                    {       
+                        clearInterval(countdownTimer);
+                        document.getElementById("timer").style.color = "maroon";    
+                    }
+        
+            }, 1000);
+
+        //clear the button and Introductory page
+        quizIntroObj.remove();
+
     });
 
 
+// select a random question from the list
+//function questionMaker
+    // create an element for the question
+    // append the
+    // for every answer
+        // create an element 
 
+// on click 
