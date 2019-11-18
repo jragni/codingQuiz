539a74f2d303a32bb18be2a21bb946ac16d345f3
Jhensen Agni <jagni001@ucr.edu>
2019-11-17 23:48:13 -0800

    Took another approach to algorithm flow. Optimized code. Created functions to clear the board, fixed bug for incorrect output.

--- script.js
+++ script.js
@@ -3,8 +3,232 @@
         clean up code
         create a function on click in which the scoreboard appears*/
 
+// Plan  17 NOV 2019
+// Create a timer function ---- complete
+// Build a button ---> complete
+    // that starts the timer function when it goes on --> complete
+    // clears the quiz area (clear function) --> complete
+
+// create a function that
+    // creates an heading element for the question
+        // innerHTML is the question
+        // appends to the quiz-box element
+    //for each choice:
+        // creates a div element for the choice
+
+//clear function 
+    // Removes all elements within the quiz area, --> done 
+    // stops timer 
 
 
+// variables
+var timeKeeper; //= setInterval(timerHandler,1000);
+var quizBox = document.getElementById("quiz-box");
+var timer = document.getElementById("timer");
+var is_TimerRunning = false;
+var startButton = document.getElementById("start-quiz");
+var totalTime = 75; // Amount of time for quiz
+var quizTime = 75; // current quiz time
+var questionNumber = 0;
+var maxScore = 75; 
+
+// Questions
+
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+        choices: ["last()","put()","push()","None of the above"],
+        answer: "push()"
+    },
+
+    {
+        title: "What is the HTML tag under which one can write the JavaScript code?",
+        choices: [ 'javascript', 'scripted', 'script', 'js'],
+        answer: 'script'
+    },
+
+    {
+        title:"Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?",
+        choices: ['alertbox(“GeeksforGeeks”);', 'msg(“GeeksforGeeks”);', 'msgbox(“GeeksforGeeks”);', 'alert(“GeeksforGeeks”);'],
+        answer:"alert(“GeeksforGeeks”);"
+    } 
+    ];
+
+
+
+
+// functions 
+
+function clearQuizBox()
+    {    
+        while(quizBox.children.length>0)
+        {
+            quizBox.children[0].remove();
+        }
+    }
+
+function timerHandler()
+    {
+        console.log("quizTime");
+        
+        quizTime = quizTime - 1;
+
+        // updates the Text on the timer element
+        timer.style.color = "white";
+        timer.innerHTML = "Time Remaining: " + quizTime + " seconds";
+        if (is_TimerRunning == true)
+        {
+            if(quizTime <= 25 && quizTime> 15)
+                {
+                    timer.style.color = "yellow";
+                }
+            if( quizTime <= 15 && quizTime > 10)
+                { 
+                    timer.style.color = "orange";
+                }
+            if( quizTime <= 10)
+                {
+                    timer.style.color = "red";
+                }
+            if( quizTime <= 0)
+            {
+                timer.innerHTML = "Time Remaing: 0 seconds"
+                timer.style.color = "maroon"
+                is_TimerRunning = false;
+                clearInterval(timeKeeper);
+                quizComplete();
+            } 
+        }
+        else{
+            clearInterval(timeKeeper);
+            quizComplete();
+           
+        }
+        
+}
+
+function poseQuestion(iteration)
+    {
+        if (questionNumber< questions.length && is_TimerRunning)
+            {
+                var quizQuestion = document.createElement("h1");
+                quizQuestion.innerHTML = questions[iteration]["title"];
+                quizQuestion.setAttribute("id","quiz-question");
+                quizQuestion.setAttribute("style","font-size: 2em");
+                quizBox.appendChild(quizQuestion);
+                for(var jj = 0 ; jj < questions[iteration]["choices"].length; jj++)
+                {        
+                    var choice = document.createElement("div");
+                    choice.innerHTML=questions[iteration]["choices"][jj];
+                    choice.setAttribute("onclick","correctSelection()")
+                    if(questions[iteration]["choices"][jj] == questions[iteration]["answer"])
+                        {
+                            choice.setAttribute("id","answer");
+                            choice.setAttribute("onclick","correctSelection()")
+
+                        }
+                    else
+                        {
+                            choice.setAttribute("id","wrong");
+                            choice.setAttribute("onclick","incorrectSelection()");
+                        }
+
+                    quizQuestion.appendChild(choice);
+                }
+            }
+        else
+            {
+                quizComplete();
+            }
+    }
+// incorrect alert
+var incorrectAlert = document.createElement("div");
+
+function incorrectSelection()
+    {
+        incorrectAlert.innerHTML = " Incorrect!" ;
+        incorrectAlert.setAttribute("id","incorrect-alert")
+        quizTime = quizTime - 15;
+        if (quizTime<0)
+            {
+                quizTime=0;
+            }
+        quizBox.appendChild(incorrectAlert);
+    }
+
+
+
+
+function correctSelection()
+    {    
+        alert("Correct!");
+        questionNumber++;
+        clearQuizBox();
+        if (is_TimerRunning)
+        {
+           poseQuestion(questionNumber);
+        }
+    }
+
+function finalScore()
+    {   
+        // time Elapsed = Total Time - quizTime 
+        // Score = MaxScore - time Elapsed
+        var timeElapsed = totalTime - quizTime;
+        var Score = maxScore - timeElapsed;
+        if(Score < 0 )
+            {
+                Score = 0;
+            }
+        var scoreObj = document.createElement("div");
+        scoreObj.innerHTML = "Final Score:"+ Score + " points!";
+        scoreObj.style.fontSize = "2em"; 
+        quizBox.appendChild(scoreObj);
+    }
+
+function quizComplete()
+    {
+        is_TimerRunning = false;
+        clearQuizBox();
+        clearInterval(timeKeeper);
+        timer.remove();
+        finalScore();
+    }
+// event listeners
+
+
+// Start Quiz
+startButton.addEventListener("click", function()
+    {
+        clearQuizBox();
+        is_TimerRunning = true;
+        timeKeeper = setInterval(timerHandler,1000);
+        poseQuestion(questionNumber);
+    });
+
+
+
+
+
+
+
+
+
+
+// Previous code for reference 
+/*        
 var startButton = document.getElementById("start-quiz");
 var Timer = document.getElementById("timer");
 var quizBox = document.getElementById("quiz-box"); // element of DOM which contains the Quiz UI
@@ -18,39 +242,39 @@ var stopTime = false;
 // Object to ask questions
 var score = 0;
 var questionNumber = 0;
-var questions = [
-                {
-                    title: "Commonly used data types DO NOT include:",
-                    choices: ["strings", "booleans", "alerts", "numbers"],
-                    answer: "alerts"
-                },
-
-                {
-                    title: "The condition in an if / else statement is enclosed within ____.",
-                    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-                    answer: "parentheses"
-                },
-
-                {
-                    title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
-                    choices: ["last()","put()","push()","None of the above"],
-                    answer: "push()"
-                },
-
-                {
-                    title: "What is the HTML tag under which one can write the JavaScript code?",
-                    choices: [ 'javascript', 'scripted', 'script', 'js'],
-                    answer: 'script'
-                },
-
-                {
-                    title:"Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?",
-                    choices: ['alertbox(“GeeksforGeeks”);', 'msg(“GeeksforGeeks”);', 'msgbox(“GeeksforGeeks”);', 'alert(“GeeksforGeeks”);'],
-                    answer:"alert(“GeeksforGeeks”);"
-                } 
-                ];
 
 
+function poseQuestion(iteration)
+    //pose Question takes the input iteration and selects that problem from the array problem and creates the elements: choice, quizQuestion  
+    {
+        // create a div element with the Question title as the inner text HTML
+        quizQuestion.innerHTML = questions[iteration]["title"];
+        quizQuestion.setAttribute("id","quiz-question");
+        quizQuestion.setAttribute("style","font-size: 2em");
+        quizBox.appendChild(quizQuestion);
+
+        for(var jj = 0 ; jj <= questions[iteration]["choices"].length; jj++)
+            {        
+                var choice = document.createElement("div");
+                choice.innerHTML=questions[iteration]["choices"][jj];
+                choice.setAttribute("onclick","correctSelection()")
+                if(questions[iteration]["choices"][jj] == questions[iteration]["answer"])
+                    {
+                        choice.setAttribute("id","answer");
+                        choice.setAttribute("onclick","correctSelection()")
+    
+                    }
+                else
+                    {
+                        choice.setAttribute("id","wrong");
+                        choice.setAttribute("onclick","incorrectSelection()");
+                    }
+
+                quizQuestion.appendChild(choice);
+
+            }
+        
+    }
 startButton.addEventListener("click",function()
     {
         var countdownTimer = setInterval(function()
@@ -131,37 +355,7 @@ function incorrectSelection()
     }
 
 
-function poseQuestion(iteration)
-    //pose Question takes the input iteration and selects that problem from the array problem and creates the elements: choice, quizQuestion  
-    {
-        // create a div element with the Question title as the inner text HTML
-        quizQuestion.innerHTML = questions[iteration]["title"];
-        quizQuestion.setAttribute("id","quiz-question");
-        quizQuestion.setAttribute("style","font-size: 2em");
-        quizBox.appendChild(quizQuestion);
 
-        for(var jj = 0 ; jj <= questions[iteration]["choices"].length-1; jj++)
-            {        
-                var choice = document.createElement("div");
-                choice.innerHTML=questions[iteration]["choices"][jj];
-                choice.setAttribute("onclick","correctSelection()")
-                if(questions[iteration]["choices"][jj] == questions[iteration]["answer"])
-                    {
-                        choice.setAttribute("id","answer");
-                        choice.setAttribute("onclick","correctSelection()")
-    
-                    }
-                else
-                    {
-                        choice.setAttribute("id","wrong");
-                        choice.setAttribute("onclick","incorrectSelection()");
-                    }
-
-                quizQuestion.appendChild(choice);
-
-            }
-        
-    }
 
 
 function clearBoard(){
